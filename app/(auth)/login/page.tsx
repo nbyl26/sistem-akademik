8926f7f391ec9f625925a07e13372509cf55bb1a
Alif Jian <alifjian834@gmail.com>
2025-11-19 08:25:17 +0700

    Feat(Login): Login with firebase

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
+import { auth } from "@/firebase/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { FirebaseError } from "firebase/app";
+import { useRouter } from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+export default function LoginPage() {
+  const [email, setEmail]: [string, React.Dispatch<SetStateAction<string>>] =
+    useState("");
+  const [password, setPassword]: [string, Dispatch<SetStateAction<string>>] =
+    useState("");
+  const [error, setError]: [string, Dispatch<SetStateAction<string>>] =
+    useState("");
+
+  const router: AppRouterInstance = useRouter();
+
+  const handleLogin = async (e: FormEvent) => {
+    e.preventDefault();
+    router.push("/dashboard");
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      if (err instanceof FirebaseError) {
+        switch (err.code) {
+          case "auth/invalid-credential":
+          case "auth/invalid-email":
+          case "auth/wrong-password":
+          case "auth/user-not-found":
+            setError("Email atau password salah");
+            break;
+
+          default:
+            break;
+        }
+      } else {
+        console.error(err);
+      }
+    }
+  };
+
+  return (
+    <div className="p-6 max-w-sm mx-auto">
+      <h1 className="text-2xl font-bold">Login</h1>
+
+      <form onSubmit={handleLogin} className="flex flex-col gap-4 mt-4">
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-2"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-2"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {error && <p className="text-red-600">{error}</p>}
+
+        <button className="bg-green-600 text-white p-2 rounded">Login</button>
+      </form>
+    </div>
+  );
+}
