c638b4299a90039f1f68831564ac437df78d57d9
Alif Jian <alifjian834@gmail.com>
2025-11-22 15:13:13 +0700

    Chore: add types Siswa,Guru, and Admin

---
+++ types/user.ts
@@ -0,0 +1,27 @@
+export type UserRole = "admin" | "guru" | "siswa";
+
+export interface BaseUser {
+  uid: string;
+  email: string;
+  role: UserRole;
+}
+
+export interface Admin extends BaseUser {
+  role: "admin";
+}
+
+export interface Guru extends BaseUser {
+  role: "guru";
+  nip: string;
+  mataPelajaran: string;
+  kelasId: string;
+}
+
+export interface Siswa extends BaseUser {
+  role: "siswa";
+  nis: string;
+  kelasId: string;
+  currentSemester: number;
+}
+
+export type User = Admin | Guru | Siswa;
