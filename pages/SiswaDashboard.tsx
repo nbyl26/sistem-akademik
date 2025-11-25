a26d8201b1d8db851ad6edfdc107c34afaa7b2ec
Nabil <nabilpasha230606@gmail.com>
2025-11-25 11:17:10 +0700

    refactor: remove old dashboard structure

--- pages/SiswaDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { Siswa } from "@/types/user";
-
-export default function SiswaDashboard({user} : {user: Siswa}){
-    return(
-        <div>
-            <h1>Hello siswa ku {user.nama}</h1>
-        </div>
-    )
-}
